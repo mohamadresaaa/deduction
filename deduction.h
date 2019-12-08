a3d782a875f3204353a72565dc9bb846e6196d30
mohamadreza mosalli <mohamadresaaa@gmail.com>
2019-12-08 19:18:01 +0330

    complete project

--- deduction.h
+++ deduction.h
@@ -2,13 +2,49 @@
 // deduction.h
 // Created by Mohamadreza Mosalli on 9/17/1398 AP.
 
-#ifndef _deduction_h_
-#define _deduction_h_
+#include <iostream>
+
+using namespace std;
 
 class deduction {
-public:
-	deduction();
-	virtual ~deduction();
-};
+private:
+	int numerator;
+	int denominator;
 
-#endif
+public:
+	void input() {
+		cout << "enter numerator: ";
+		cin >> this->numerator;
+		cout << "enter denominator: ";
+		cin >> this-> denominator;
+	}
+	void show() {
+		cout << " " << this->numerator << "\n";
+		cout << "---" << "\n";
+		cout << " " << this->denominator << "\n";
+	}
+	void sum(deduction obj) {
+		if(this->denominator != obj.denominator) {
+			this->numerator *= obj.denominator;
+			obj.numerator *= this->denominator;
+		}
+		this->denominator *= obj.denominator;
+		this->numerator += obj.numerator;
+	}
+	void minus(deduction obj) {
+		if(this->denominator != obj.denominator) {
+			this->numerator *= obj.denominator;
+			obj.numerator *= this->denominator;
+		}
+		this->denominator -= obj.denominator;
+		this->numerator -= obj.numerator;
+	}
+	void multiplication(deduction obj) {
+		this->denominator *= obj.denominator;
+		this->numerator *= obj.numerator;
+	}
+	void division(deduction obj) {
+		this->denominator *= obj.numerator;
+		this->numerator *= obj.denominator;
+	}
+};
--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "deduction.h"
 
 using namespace std;
 int main(int argc, char *argv[]) {
+	deduction d1,d2;
+	d1.input();
+	d2.input();
+	d1.
+	d1.show();
 	
+	return 0;
 }
