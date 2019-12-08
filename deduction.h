b82e46552b0b3fb190844024643da60c4019996c
mohamadreza mosalli <mohamadresaaa@gmail.com>
2019-12-08 18:04:20 +0330

    add deduction class

---
+++ deduction.h
@@ -0,0 +1,14 @@
+//
+// deduction.h
+// Created by Mohamadreza Mosalli on 9/17/1398 AP.
+
+#ifndef _deduction_h_
+#define _deduction_h_
+
+class deduction {
+public:
+	deduction();
+	virtual ~deduction();
+};
+
+#endif
